ca9e77c7380aadb04c62edb9e1c69e3aeb14f442
Josh <partisansenator@gmail.com>
2014-03-18 04:22:25 -0700

    Added in some extra back-end functionality.  create user function, add user to list function, and multi-list functionality all work in terms of the back-end now.

--- GroceryGoblin/GGViewController.m
+++ GroceryGoblin/GGViewController.m
@@ -15,92 +15,52 @@
 @end
 
 @implementation GGViewController
-{
-    
-// FIXME: remove this temp variable or turn it into a private property
-NSMutableArray *_shoppingItems;
-}
+
 
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-    // FIXME: don't access _variable, use the getter
-  //  _shoppingItems = [[NSMutableArray alloc] init];
- //   PFObject *item = [PFObject objectWithClassName:@"ListItem"];
-  //  [item setObject:[PFUser currentUser] forKey:@"createdBy"]
-  //  [_shoppingItems addObject:[GGShoppingItem shoppingItemWithName:@"Milk" andQuantity:@3]];
-  //  [_shoppingItems addObject:[GGShoppingItem shoppingItemWithName:@"Honey" andQuantity:@2]];
-   // [_shoppingItems addObject:[GGShoppingItem shoppingItemWithName:@"Butter" andQuantity:@40]];
-    
-    
     
     
     self.displayList = [[NSMutableArray alloc] init];
-    PFUser *user;
+    PFUser *user = [PFUser user];
     if([PFUser currentUser] != NULL){
         user = [PFUser currentUser];
     }else{
         user = [PFUser logInWithUsername:@"Josh" password:@"plaintextfolyfe"];
     }
+   
+  //  [self createNewUserWithName:@"Morgan" andPass:@"iloveboobies"];
+   // [self createNewUserWithName:@"Evan" andPass:@"iALSOloveboobies!"];
     
     PFQuery *query = [PFQuery queryWithClassName:@"ShoppingList"];
-    PFQuery *itemQuery = [PFQuery queryWithClassName:@"ListItem"];
-    [itemQuery orderByDescending:@"createdAt"];
-    [query whereKey:@"createdBy" equalTo:user];
-    [query includeKey:@"Items"];
-    //PFObject *list = [query findObjects];
-    NSArray* lists = [query findObjects];
-    self.list = [lists objectAtIndex:0];
+    [query whereKey:@"users" equalTo:user.objectId];
+    
+    NSArray *lists = [query findObjects];
+    for(PFObject *list in lists){
+        NSLog(@"Hey, here's a list! %@", list.objectId);
+    }
+    
+    //MORGAN AND EVAN - CHANGE THE INDEX HERE TO GET THE ITEMS FOR DIFFERENT LISTS THE USER HAS ACCESS TO
+    self.list = [lists objectAtIndex:2];
 
+    PFQuery *itemQuery = [PFQuery queryWithClassName:@"ListItem"];
+    [itemQuery orderByDescending:@"createdAt"];    
     [itemQuery whereKey:@"ShoppingList" equalTo:self.list];
     NSArray *list2 = [itemQuery findObjects];
     
     
-//    PFObject *testList = [PFObject objectWithClassName:@"ShoppingList"];
-//    [testList setObject:user forKey:@"createdBy"];
-//    self.list = testList;
-//    
-//    
-//    PFObject *testItem1 = [PFObject objectWithClassName:@"ListItem"];
-//    PFObject *testItem2 = [PFObject objectWithClassName:@"ListItem"];
-//    PFObject *testItem3 = [PFObject objectWithClassName:@"ListItem"];
-//    PFObject *testItem4 = [PFObject objectWithClassName:@"ListItem"];
-//    PFObject *testItem5 = [PFObject objectWithClassName:@"ListItem"];
-//    testItem1[@"name"] = @"butter";
-//    testItem1[@"quantity"] = @1;
-//    //[testItem1 setObject:self.list forKey:@"ShoppingList"];
-//    testItem2[@"name"] = @"milk";
-//    testItem2[@"quantity"] = @1;
-//    //[testItem2 setObject:self.list forKey:@"ShoppingList"];
-//    testItem3[@"name"] = @"eggs";
-//    testItem3[@"quantity"] = @1;
-//    //[testItem3 setObject:self.list forKey:@"ShoppingList"];
-//    testItem4[@"name"] = @"beer";
-//    testItem4[@"quantity"] = @1;
-//    //[testItem4 setObject:self.list forKey:@"ShoppingList"];
-//    testItem5[@"name"] = @"soda";
-//    testItem5[@"quantity"] = @1;
-//    //[testItem5 setObject:self.list forKey:@"ShoppingList"];
-//    [self.list saveInBackground];
-//    NSArray *items = @[testItem1, testItem2, testItem3, testItem4, testItem5];
-//    [self.list setObject:items forKey:@"Items"];
-//    [testItem1 saveInBackground];
-//    [testItem2 saveInBackground];
-//    [testItem3 saveInBackground];
-//    [testItem4 saveInBackground];
-//    [testItem5 saveInBackground];
-//    [self.list saveInBackground];
-//    
-    
-    
+    //HERE'S SOME SAMPLE CODE FOR ADDING A USER.  Modify the query accordingly.
+    PFQuery *userQuery = [PFUser query];
+    [userQuery whereKey:@"username" equalTo:@"Morgan"];
+    PFUser *secondUser = [PFUser user];
+    secondUser = [[userQuery findObjects] objectAtIndex:0];
+    [self addUser:secondUser toList:self.list];
     
     
 
     
-    
-    //self.list[@"Items"] =
     NSLog(@"%@", [self.list objectId]);
     self.items = [self.list objectForKey:@"Items"];
     for(PFObject *item in list2){
@@ -111,7 +71,7 @@ NSMutableArray *_shoppingItems;
     }
     
 
-    
+
     
     self.itemsTableView.dataSource = self;
     [self.itemsTableView registerClass:[GGCell class] forCellReuseIdentifier:@"cell"];
@@ -125,14 +85,6 @@ NSMutableArray *_shoppingItems;
 
 
 
-//    PFObject *testList = [PFObject objectWithClassName:@"ShoppingList"];
-//    [testList setObject:user forKey:@"createdBy"];
-//    PFObject *testItem1 = [PFObject objectWithClassName:@"ListItem"];
-    //    testItem1[@"name"] = @"butter";
-    //    testItem1[@"quantity"] = @1;
-    
-//    [testList saveInBackground];
-   // [testItem1 saveInBackground];
 }
 
 - (void)didReceiveMemoryWarning
@@ -192,11 +144,6 @@ NSMutableArray *_shoppingItems;
     [newItem setObject:self.list forKey:@"ShoppingList"];
     shopItem.itemParseObject = newItem;
     [newItem saveInBackground];
-  //  [self.items addObject:newItem];
-    //self.list[@"Items"] = self.items;
-  //  [self.list setObject:self.items forKey:@"Items"];
-    //[newItem saveInBackground];
-  //  [self.list saveInBackground];
     
     [self.itemsTableView insertRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:0 inSection:0]]withRowAnimation:UITableViewRowAnimationTop];
     [self.itemsTableView endUpdates];
@@ -213,5 +160,37 @@ NSMutableArray *_shoppingItems;
     GGShoppingItem *item = [[GGShoppingItem alloc] initItemWithName:sender.text andQuantity:@1];
     [self shoppingItemAdded:item];
     self.createNewItemTextField.text = @"";
+}
+
+
+-(void) addUser:(PFUser *)user toList:(PFObject *)list{
+    [list addObject:user.objectId forKey:@"users"];
+    
+}
+
+-(PFObject *) makeNewListForUser:(PFUser *)user withListName:(NSString *)listName{
+    PFObject *newList = [PFObject objectWithClassName:@"ShoppingList"];
+    [newList setObject:user forKey:@"createdBy"];
+    [newList setObject:listName forKey:@"ListName"];
+    [newList setObject:@[user.objectId] forKey:@"users"];
+    [newList saveInBackground];
+    return newList;
+}
+
+-(void)createNewUserWithName:(NSString*)name andPass:(NSString*)pass{
+    PFUser *user = [PFUser user];
+    user.username = name;
+    user.password = pass;
+
+
+    [user signUpInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if (!error) {
+        // Hooray! Let them use the app now.
+        } else {
+            NSString *errorString = [error userInfo][@"error"];
+            // Show the errorString somewhere and let the user try again.
+        }
+    }];
+    
 }
 @end
